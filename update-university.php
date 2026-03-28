a0f7e1d52ed3f1d4188ec7ae7afca28262087109
mandalraman277-crypto <mandalraman277@gmail.com>
2026-03-28 16:04:51 +0000

    Add files via upload

---
+++ update-university.php
@@ -0,0 +1,25 @@
+<?php
+include 'db.php';
+
+
+$university_id = $_POST['university_id'];
+$university_name = $_POST['university_name'];
+$university_description = $_POST['university_description'];
+$rating = $_POST['rating'];
+$established_date = $_POST['established_date'];
+
+// Update query
+$sql = "UPDATE University 
+        SET university_name='$university_name',
+            university_description='$university_description',
+            rating='$rating',
+            established_date='$established_date'
+        WHERE university_id=$university_id";
+
+mysqli_query($mysqli, $sql);
+
+// Redirect back to list
+header("Location: list-university.php");
+exit;
+?>
+
